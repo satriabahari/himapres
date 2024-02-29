783523b767cfa092afc018b737dc3ae5fee0a389
Muktashim Billah <muktasim.elfatih@gmail.com>
2024-02-29 11:16:17 +0700

    fix datamaster and add datahimasi

--- resources/views/livewire/pages/users/list-users.blade.php
+++ resources/views/livewire/pages/users/list-users.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
 
     <x-slot name='breadcrumb'>
-        <livewire:components.widget.breadcrumb breadcrumb="{{ __($breadcrumb) }}"/>
+        <livewire:components.widget.breadcrumb breadcrumb="{{ __($breadcrumb) }}" />
     </x-slot>
 
     <!-- Row -->
@@ -17,7 +17,7 @@
 
                 <div class="card-body">
                     <div class="table-responsive text-center">
-                        <table class="table table-bordered border text-nowrap mb-0 " id="new-edit">
+                        <table class="table table-bordered border text-nowrap mb-0 " id="myTable">
                             <thead class="text-center">
                                 <tr>
                                     <th>No</th>
@@ -29,22 +29,18 @@
                             </thead>
                             <tbody>
                                 @foreach ($users as $user)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $user->name }}</td>
-                                        <td>{{ $user->email }}</td>
-                                        <td>{{ $user->created_at }}</td>
-                                        <td class="d-flex justify-content-center border-0">
-                                            <a href="{{ route('admin.users.show', $user->id) }}"
-                                                class="btn btn-sm btn-primary badge  mx-2" wire:navigate><i
-                                                    class="fe fe-edit"></i></a>
-                                            <form wire:submit='destroy({{$user->id}})'
-                                                class="inline-block" wire:confirm="Yakin Ingin Menghapus?">
-                                                <button class="btn btn-sm btn-danger badge " type="submit"
-                                                    name="action" ><i class="fa fa-trash"></i></button>
-                                            </form>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->created_at }}</td>
+                                    <td class="d-flex justify-content-center border-0">
+                                        <a href="{{ route('admin.users.show', $user->id) }}" class="btn btn-sm btn-primary badge  mx-2" wire:navigate><i class="fe fe-edit"></i></a>
+                                        <form wire:submit='destroy({{$user->id}})' class="inline-block" wire:confirm="Yakin Ingin Menghapus?">
+                                            <button class="btn btn-sm btn-danger badge " type="submit" name="action"><i class="fa fa-trash"></i></button>
+                                        </form>
+                                    </td>
+                                </tr>
                                 @endforeach
 
                             </tbody>
