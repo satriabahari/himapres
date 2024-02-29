246953bcaad3e19597037ecef4c46d6b538dda76
Muktashim Billah <muktasim.elfatih@gmail.com>
2024-02-29 11:22:21 +0700

    comit sementara

--- resources/views/livewire/pages/posisi/listposisi.blade.php
+++ resources/views/livewire/pages/posisi/listposisi.blade.php
@@ -4,19 +4,20 @@
     </x-slot>
 
     <x-slot name='breadcrumb'>
-        <livewire:components.widget.breadcrumb breadcrumb="{{ __($breadcrumb) }}"/>
+        <livewire:components.widget.breadcrumb breadcrumb="{{ __($breadcrumb) }}" />
     </x-slot>
 
-      <!-- Row -->
-      <div class="row">
+    <!-- Row -->
+    <div class="row">
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Posisi</h3>
+                    <h3 class="card-title" style="width: -webkit-fill-available;">Posisi</h3>
+                    <a id="table2-new-row-button " class="btn btn-primary" href="{{route('admin.posisi.create')}}" wire:navigate>Add New </a>
                 </div>
 
                 <div class="card-body">
-                    <a  id="table2-new-row-button " class="btn btn-primary mb-4 "  href="{{route('admin.posisi.create')}}" wire:navigate>Add New </a>
+
                     <div class="table-responsive text-center">
                         <table class="table table-bordered border text-nowrap mb-0 " id="new-edit">
                             <thead class="text-center">
@@ -30,19 +31,17 @@
                             <tbody>
                                 </tr>
                                 @foreach ($data as $dt )
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $dt->name }}</td>
-                                        <td>{{ $dt->created_at }}</td>
-                                        <td class="d-flex justify-content-center border-0">
-                                            <a href="{{ route('admin.posisi.edit',$dt->id) }}"
-                                            class="btn btn-sm btn-primary badge  mx-2" wire:navigate><i
-                                                class="fe fe-edit"></i></a>
-                                                <button class="btn btn-sm btn-danger badge " wire:click="delete({{ $dt->id }})" wire:confirm="Yakin Ingin Menghapus!">
-                                                    <i class="fa fa-trash"></i>
-                                                </button>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $dt->name }}</td>
+                                    <td>{{ $dt->created_at }}</td>
+                                    <td class="d-flex justify-content-center border-0">
+                                        <a href="{{ route('admin.posisi.edit',$dt->id) }}" class="btn btn-sm btn-primary badge  mx-2" wire:navigate><i class="fe fe-edit"></i></a>
+                                        <button class="btn btn-sm btn-danger badge " wire:click="delete({{ $dt->id }})" wire:confirm="Yakin Ingin Menghapus!">
+                                            <i class="fa fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
                                 @endforeach
 
 
