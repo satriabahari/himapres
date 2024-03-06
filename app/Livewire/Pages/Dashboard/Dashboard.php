84a5ed38d33880e4c74a54ceb820f655fd308596
Muktashim Billah <muktasim.elfatih@gmail.com>
2024-03-06 15:06:13 +0700

    dasboard web absensi

--- app/Livewire/Pages/Dashboard/Dashboard.php
+++ app/Livewire/Pages/Dashboard/Dashboard.php
@@ -2,16 +2,29 @@
 
 namespace App\Livewire\Pages\Dashboard;
 
+use App\Models\ModelMhs;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
-    public $breadcrumb ="Dashboard";
-    
-    public $title ="Dashboard";
+    public $breadcrumb = "Dashboard";
+    public $countAnggota;
+    public $countMahasiswa;
+    public $countDosen;
+    public $countTamu;
+
+    public $title = "Dashboard";
 
     public function render()
     {
         return view('livewire.pages.dashboard.dashboard');
+    }
+
+    public function mount()
+    {
+        $this->countAnggota = ModelMhs::all()->count();
+        $this->countMahasiswa = ModelMhs::where('jabatan', 'Mahasiswa')->count();
+        $this->countDosen = ModelMhs::where('jabatan', 'Dosen')->count();
+        $this->countTamu = ModelMhs::where('jabatan', 'Tamu')->count();
     }
 }
--- resources/views/livewire/pages/dashboard/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard/dashboard.blade.php
@@ -4,11 +4,11 @@
     </x-slot>
 
     <x-slot name='breadcrumb'>
-        <livewire:components.widget.breadcrumb breadcrumb="{{ __($breadcrumb) }}"/>
+        <livewire:components.widget.breadcrumb breadcrumb="{{ __($breadcrumb) }}" />
     </x-slot>
     <!-- ROW-1 -->
 
-     <div class="row">
+    <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12">
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
@@ -16,19 +16,17 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="mt-2">
-                                    <h6 class="">Total Users</h6>
-                                    <h2 class="mb-0 number-font">44,278</h2>
+                                    <h6 class="">Total Anggota</h6>
+                                    <h2 class="mb-0 number-font">{{ $countAnggota}}</h2>
                                 </div>
                                 <div class="ms-auto">
                                     <div class="chart-wrapper mt-1">
-                                        <canvas id="saleschart"
-                                            class="h-8 w-9 chart-dropshadow"></canvas>
+                                        <canvas id="saleschart" class="h-8 w-9 chart-dropshadow"></canvas>
                                     </div>
                                 </div>
                             </div>
-                            <span class="text-muted fs-12"><span class="text-secondary"><i
-                                        class="fe fe-arrow-up-circle  text-secondary"></i> 5%</span>
-                                Last week</span>
+                            <span class="text-muted fs-12"><span class="text-secondary"><i class="fe fe-arrow-up-circle  text-secondary"></i> 100%</span>
+                                Form data Angota</span>
                         </div>
                     </div>
                 </div>
@@ -37,19 +35,17 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="mt-2">
-                                    <h6 class="">Total Profit</h6>
-                                    <h2 class="mb-0 number-font">67,987</h2>
+                                    <h6 class="">Total Mahasiswa</h6>
+                                    <h2 class="mb-0 number-font">{{ $countMahasiswa}}</h2>
                                 </div>
                                 <div class="ms-auto">
                                     <div class="chart-wrapper mt-1">
-                                        <canvas id="leadschart"
-                                            class="h-8 w-9 chart-dropshadow"></canvas>
+                                        <canvas id="leadschart" class="h-8 w-9 chart-dropshadow"></canvas>
                                     </div>
                                 </div>
                             </div>
-                            <span class="text-muted fs-12"><span class="text-pink"><i
-                                        class="fe fe-arrow-down-circle text-pink"></i> 0.75%</span>
-                                Last 6 days</span>
+                            <span class="text-muted fs-12"><span class="text-pink"><i class="fe fe-arrow-down-circle text-pink"></i> {{ number_format($countMahasiswa/$countAnggota*100, 2);}}%</span>
+                                Form data Angota</span>
                         </div>
                     </div>
                 </div>
@@ -58,19 +54,17 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="mt-2">
-                                    <h6 class="">Total Expenses</h6>
-                                    <h2 class="mb-0 number-font">$76,965</h2>
+                                    <h6 class="">Total Dosen</h6>
+                                    <h2 class="mb-0 number-font">{{ $countDosen}}</h2>
                                 </div>
                                 <div class="ms-auto">
                                     <div class="chart-wrapper mt-1">
-                                        <canvas id="profitchart"
-                                            class="h-8 w-9 chart-dropshadow"></canvas>
+                                        <canvas id="profitchart" class="h-8 w-9 chart-dropshadow"></canvas>
                                     </div>
                                 </div>
                             </div>
-                            <span class="text-muted fs-12"><span class="text-green"><i
-                                        class="fe fe-arrow-up-circle text-green"></i> 0.9%</span>
-                                Last 9 days</span>
+                            <span class="text-muted fs-12"><span class="text-green"><i class="fe fe-arrow-up-circle text-green"></i> {{ number_format($countDosen/$countAnggota*100, 2);}}%</span>
+                                Form data Angota</span>
                         </div>
                     </div>
                 </div>
@@ -79,19 +73,17 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="mt-2">
-                                    <h6 class="">Total Cost</h6>
-                                    <h2 class="mb-0 number-font">$59,765</h2>
+                                    <h6 class="">Total Tamu</h6>
+                                    <h2 class="mb-0 number-font">{{ $countTamu}}</h2>
                                 </div>
                                 <div class="ms-auto">
                                     <div class="chart-wrapper mt-1">
-                                        <canvas id="costchart"
-                                            class="h-8 w-9 chart-dropshadow"></canvas>
+                                        <canvas id="costchart" class="h-8 w-9 chart-dropshadow"></canvas>
                                     </div>
                                 </div>
                             </div>
-                            <span class="text-muted fs-12"><span class="text-warning"><i
-                                        class="fe fe-arrow-up-circle text-warning"></i> 0.6%</span>
-                                Last year</span>
+                            <span class="text-muted fs-12"><span class="text-warning"><i class="fe fe-arrow-up-circle text-warning"></i> {{ number_format($countTamu/$countAnggota*100, 2);}}%</span>
+                                Form data Angota</span>
                         </div>
                     </div>
                 </div>
