23cd83f186d2f34fed89ac5c9aa974d65bcc0b2f
yotadaa <mukhtadanasution@gmail.com>
2025-01-23 21:59:19 +0700

    fix rekap

--- app/Http/Controllers/Help/Rekap.php
+++ app/Http/Controllers/Help/Rekap.php
@@ -11,27 +11,63 @@ use App\Models\ModelPesertaEvent;
 class Rekap extends Controller
 {
     //
-    public function index() {
+    public function index()
+    {
         $event = $this->getAcara('21010015');
-        echo $event;
+        foreach ($event as $ev) {
+            echo $ev . '<br>';
+        }
     }
 
-    public function getAcara($nim) {
+    public function getAcara($nim)
+    {
         $mhs = $this->getMahasiswa($nim);
+
         $result = ModelPesertaEvent::where('mhs_id', $mhs->id)
             ->join('events', 'peserta_event.event_id', '=', 'events.id')
-            ->select('events.name_event', 'events.date_start', 'events.date_end', 'events.detail')
+            ->join('data_posisi', 'peserta_event.posisi', '=', 'data_posisi.id')
+            ->leftJoin('absensi', function ($join) {
+                $join->on('absensi.event_id', '=', 'events.id');
+            })
+            ->leftJoin('data_kehadiran', function ($join) {
+                $join->on('data_kehadiran.absensi_id', '=', 'absensi.id')->on('data_kehadiran.peserta_id', '=', 'peserta_event.id');
+            })
+            ->select(
+                'absensi.title',
+                'events.name_event',
+                'data_posisi.name AS position_name',
+                'events.detail',
+                'events.date_start',
+                'events.date_end',
+                'absensi.created_at AS waktu',
+                'data_kehadiran.status', // Assuming 'status' in data_kehadiran table
+            )
             ->get();
-        return $result; 
+
+        return $result;
     }
 
-    public function getMahasiswa($nim) {
+    public function getMahasiswa($nim)
+    {
         $mhs = ModelMhs::where('nim', $nim)->first();
         return $mhs;
     }
 
-    public function byNim($nim) {
+    public function byNim($nim)
+    {
         $event = $this->getAcara($nim);
-        echo $event;
+        foreach ($event as $ev) {
+            echo $ev . '<br>';
+        }
+    }
+
+    public function download($nim)
+    {
+        $sheet1 = $this->getAcara($nim)->map(function ($item, $i) {
+            return [$i + 1, $item['name_event'], $item];
+        });
+
+        // Export to Excel
+        return Excel::download(new TemplateExport($data), 'Data Panitia ' . $this->dataEvent->name_event . '.xlsx');
     }
 }
